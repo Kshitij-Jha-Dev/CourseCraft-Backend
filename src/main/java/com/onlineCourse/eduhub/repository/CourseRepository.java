5691d2d25f266145b3590f8536e0ff74de18ac03
Kshitj Jha <kj@Kshitjs-MacBook-Air.local>
2026-01-27 12:08:56 +0530

    Added search and all all courses endpoints

--- src/main/java/com/onlineCourse/eduhub/repository/CourseRepository.java
+++ src/main/java/com/onlineCourse/eduhub/repository/CourseRepository.java
@@ -2,6 +2,18 @@ package com.onlineCourse.eduhub.repository;
 
 import com.onlineCourse.eduhub.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+    @Query("""
+        SELECT c FROM Course c
+        WHERE lower(c.courseName) LIKE lower(concat('%', :keyword, '%'))
+           OR lower(c.courseDescription) LIKE lower(concat('%', :keyword, '%'))
+           OR lower(c.trainer) LIKE lower(concat('%', :keyword, '%'))
+    """)
+    List<Course> searchCourses(@Param("keyword") String keyword);
 }
