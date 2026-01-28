7a6941423522ff8179c34c572d5aa0a9dfed88ab
Kshitj Jha <kj@Kshitjs-MacBook-Air.local>
2026-01-28 21:17:02 +0530

    Added trainer entity and relevant CRUD endpoints

---
+++ src/main/java/com/onlineCourse/eduhub/service/TrainerService.java
@@ -0,0 +1,16 @@
+package com.onlineCourse.eduhub.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.onlineCourse.eduhub.dto.trainer.TrainerRequest;
+import com.onlineCourse.eduhub.entity.Trainer;
+
+public interface TrainerService {
+
+    List<Trainer> getAllTrainers();
+    Trainer getTrainerById(Integer id);
+    Trainer createTrainer(TrainerRequest dto);
+    Trainer updateTrainer(Integer id, TrainerRequest dto);
+    void deleteTrainer(Integer id);
+}
